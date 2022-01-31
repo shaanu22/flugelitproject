446bedb7a2dd8c3ad2bd5378f6a6152c42edee5a
shaanu22 <mails2samtaiwo@gmail.com>
2022-01-31 20:13:09 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This terraform module creates an S3 bucket, uploads files onto the bucket, and creates timestamps of the time of execution of the terraform configuration.# flugelitproject
---
+++ variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  description = "This is the AWS region where the resources will be created."
+  default     = "us-east-1"
+}
+
+variable "bucket_prefix" {
+    type        = string
+    description = "This creates a unique bucket name beginning with the specified prefix"
+    default     = "terra-s3bucket-"
+}
+
+variable "tags" {
+    type        = map
+    description = "(Optional) A mapping of tags to assign to the S3 bucket."
+    default     = {
+        environment = "DEV"
+        terraform   = "true"
+    }
+}
+
+variable "versioning" {
+    type        = bool
+    description = "This is an optional state of versioning."
+    default     = true
+}
+
+variable "acl" {
+    type        = string
+    description = " This defaults to private "
+    default     = "private"
+}
