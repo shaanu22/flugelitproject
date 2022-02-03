e52cb58adc8cad313661dfd2b77f53c8f326c954
ADMIN <mails2samtaiwo@gmail.com>
2022-02-03 12:58:06 -0600

    my second branch commit

--- s3resources.tf
+++ s3resources.tf
@@ -4,21 +4,21 @@ resource "aws_s3_bucket" "terra-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
 
-# Enable versioning of the bucket to be created 
-   versioning {
-    enabled = var.versioning
+# Enable versioning of the bucket to be created
+versioning {
+  enabled = var.versioning
   }
 # You can choose to tag the bucket  
-  tags = var.tags
+tags = var.tags
 }
 
 # Upload multiple objects into the newly created S3 bucket
-  resource "aws_s3_bucket_object" "multiobject" {
-    bucket = aws_s3_bucket.terra-s3-bucket.id
-    for_each = fileset("/home/ec2-user/Documents/flugel-terraform-s3/test-file","*")
-    key      = each.value
-    source   = "/home/ec2-user/Documents/flugel-terraform-s3/test-file/${each.value}"
-    etag = filemd5("/home/ec2-user/Documents/flugel-terraform-s3/test-file/${each.value}")
+resource "aws_s3_bucket_object" "multiobject" {
+  bucket = aws_s3_bucket.terra-s3-bucket.id
+  for_each = fileset("/home/ec2-user/Documents/flugel-terraform-s3/test-file","*")
+  key      = each.value
+  source   = "/home/ec2-user/Documents/flugel-terraform-s3/test-file/${each.value}"
+  etag = filemd5("/home/ec2-user/Documents/flugel-terraform-s3/test-file/${each.value}")
 
      
   provisioner "local-exec" {
