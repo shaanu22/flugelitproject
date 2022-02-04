c7554c5e575f5296096bedbe3954746afc94b30c
ADMIN <mails2samtaiwo@gmail.com>
2022-02-04 01:12:11 -0600

    another little edit

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 # Specify the provider and access details
 provider "aws" {
-  region = var.aws_region
+  region     = var.aws_region
   access_key = var.AWS_ACCESS_KEY_ID
   secret_key = var.AWS_SECRET_ACCESS_KEY
 }
